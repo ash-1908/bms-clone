647f3cfb3a652acc589ef5aca3a4de29d4f4a127
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-10 11:55:53 +0530

    installed router, layout structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { DefaultHOC } from "./HOC/Default.HOC"
+
+function App() {
+  return (
+    <>
+      <DefaultHOC/>
+    </>
+  )
+}
+
+export default App
---
+++ src/HOC/Default.HOC.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../layouts/Default.layout'
+
+export const DefaultHOC = () => {
+    return (
+        <div>
+            <h2>Hello default hoc</h2>
+            <DefaultLayout/>            
+        </div>
+    )
+}
+
