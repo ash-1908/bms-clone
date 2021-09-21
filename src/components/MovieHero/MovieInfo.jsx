958ca5cac9d53fa42230fa58976b6473bae750b5
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-21 15:48:06 +0530

    created payment modal component

--- src/components/MovieHero/MovieInfo.jsx
+++ src/components/MovieHero/MovieInfo.jsx
@@ -1,16 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BsFillHeartFill } from "react-icons/bs";
 import { MovieContext } from "../../Context/Movie.context";
+import PaymentModal from "../PaymentModal/PaymentModal";
 
 const MovieInfo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [price, setPrice] = useState(0);
+
   const { movie } = useContext(MovieContext);
   const spoken_languages = movie.spoken_languages
     ?.map(({ name }) => name)
     .join(", ");
   const genres = movie.genres?.map(({ name }) => name).join(", ");
 
+  const rentMovie = () => {
+    setIsOpen(true);
+    setPrice(199);
+  };
+  const buyMovie = () => {
+    setIsOpen(true);
+    setPrice(599);
+  };
+
   return (
     <>
+      <PaymentModal isOpen={isOpen} setIsOpen={setIsOpen} price={price} setPrice={setPrice} />
       <div className="flex flex-col items-start text-white flex-col-reverse lg:flex-col">
         <h2 className="hidden lg:block text-4xl font-bold mt-2 mb-10">
           {movie.original_title}
@@ -28,16 +42,20 @@ const MovieInfo = () => {
         </div>
         <div className="mb-4 lg:mb-10 text-sm lg:text-lg font-semibold">
           <p>
-            {(movie.runtime / 60).toFixed(0) + " h " + (movie.runtime % 60).toFixed(0)+ " m"} &bull; {genres} &bull;{" "}
-            {movie.adult ? "13+" : "U"} &bull; {movie.release_date}
+            {(movie.runtime / 60).toFixed(0) +
+              " h " +
+              (movie.runtime % 60).toFixed(0) +
+              " m"}{" "}
+            &bull; {genres} &bull; {movie.adult ? "13+" : "U"} &bull;{" "}
+            {movie.release_date}
           </p>
         </div>
         <div className="flex w-full mb-4 w-full">
-          <button className="mr-2 py-2 w-full lg:px-7 lg:py-4 bg-red-600 text-lg text-white font-semibold rounded-md lg:w-44 hover:bg-red-700">
-            Buy
+          <button className="mr-2 py-2 w-full lg:px-7 lg:py-4 bg-red-600 text-lg text-white font-semibold rounded-md lg:w-44 hover:bg-red-700" onClick={buyMovie}>
+            Buy Rs 599
           </button>
-          <button className="py-2 w-full lg:px-7 lg:py-4 bg-red-600 text-lg text-white font-semibold rounded-md lg:w-44 hover:bg-red-700">
-            Rent
+          <button className="py-2 w-full lg:px-7 lg:py-4 bg-red-600 text-lg text-white font-semibold rounded-md lg:w-44 hover:bg-red-700" onClick={rentMovie}>
+            Rent Rs 199
           </button>
         </div>
       </div>
