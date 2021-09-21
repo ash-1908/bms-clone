d73bab625eae607e2cfb658c760a98d8e85d22bb
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-21 16:57:13 +0530

    removed temp folder

--- src/components/TempImages/TempBackground.js
+++
@@ -1,3 +0,0 @@
-export const image = {
-  src: "https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/zack-snyder-s-justice-league-et00047164-10-04-2021-03-22-49.jpg",
-};
